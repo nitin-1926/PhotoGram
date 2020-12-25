f3f97b96182e4ebd2742d7868243c1428bcafc85
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-12-25 19:38:29 +0530

    Adding Middleware

--- server/app.js
+++ server/app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+const customMiddleware = (req, res, next) => {
+    console.log(' Middleware Executed');
+    next();
+};
+
+app.use(customMiddleware);
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
