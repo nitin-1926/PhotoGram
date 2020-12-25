6ec4078528ef7e37d7ff9deceebc75586cb53be4
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-12-25 19:48:05 +0530

    Adding Middleware options

--- server/app.js
+++ server/app.js
@@ -6,11 +6,20 @@ const customMiddleware = (req, res, next) => {
     console.log(' Middleware Executed');
     next();
 };
-
+// If MiddleWare written here then it's executed before each route. If you want it for specific routes then remove it and add middleware to those routes only.
 app.use(customMiddleware);
+// app.get('/', customMiddleware, (req, res) => {
+//     console.log('Home');
+//     res.send('Hello World');
+// });
 
 app.get('/', (req, res) => {
+    console.log('Home');
     res.send('Hello World');
+});
+app.get('/about', (req, res) => {
+    console.log('About');
+    res.send('About Page');
 });
 
 app.listen(PORT, () => {
