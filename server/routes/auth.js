85a0eada47fe0774213b62429904fd161c081f5b
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-12-26 17:59:21 +0530

    Making signup route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,4 +5,18 @@ router.get('/', (req, res) => {
     res.send('Hello');
 });
 
+router.post('/api/signup', (req, res) => {
+    const { name, emailId, password } = req.body;
+    if (!name || !emailId || !password) {
+        res.status(422).json({
+            message: 'SignUp Failed',
+            error: 'Please add all the fields'
+        });
+    } else {
+        res.json({
+            message: 'Sign Up Successful',
+        });
+    }
+})
+
 module.exports = router;
