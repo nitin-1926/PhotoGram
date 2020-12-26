f7626bbaf79da2943952c9b7395b9fcafae1d49e
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-12-26 12:11:34 +0530

    Adding MongoDB

--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = 5000;
+const { MONGO_URI } = require('./keys');
 
-const customMiddleware = (req, res, next) => {
-    console.log(' Middleware Executed');
-    next();
-};
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+mongoose.connection.on('error', err => {
+    console.log('Error Connecting to MongoDB', err);
+});
 // If MiddleWare written here then it's executed before each route. If you want it for specific routes then remove it and add middleware to those routes only.
-app.use(customMiddleware);
+// app.use(customMiddleware);
 // app.get('/', customMiddleware, (req, res) => {
 //     console.log('Home');
 //     res.send('Hello World');
