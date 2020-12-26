070040f22b510dbf8e5869bb6c2a219f1cbdf49c
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-12-26 18:16:34 +0530

    SignUp flow route

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    email: {
+    emailId: {
         type: String,
         required: true
     },
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 router.get('/', (req, res) => {
     res.send('Hello');
@@ -8,14 +10,39 @@ router.get('/', (req, res) => {
 router.post('/api/signup', (req, res) => {
     const { name, emailId, password } = req.body;
     if (!name || !emailId || !password) {
-        res.status(422).json({
+        return res.status(422).json({
             message: 'SignUp Failed',
             error: 'Please add all the fields'
         });
     } else {
-        res.json({
-            message: 'Sign Up Successful',
-        });
+        User.findOne({ emailId: emailId })
+            .then(user => {
+                if (user) {
+                    return res.status(422).json({
+                        message: 'SignUp Failed',
+                        error: 'User already exists'
+                    });
+                } else {
+                    const user = new User({
+                        name,
+                        emailId,
+                        password
+                    });
+                    user.save()
+                        .then(user => {
+                            if (user) {
+                                return res.json({
+                                    message: 'SignUp Successful',
+                                });
+                            }
+                        }).catch(err => {
+                            console.log(err);
+                        });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 })
 
