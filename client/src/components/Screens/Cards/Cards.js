488874ea4213ece9571762d2dc5c25b2512a9c48
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2021-03-22 22:31:19 +0530

    Hyperlinks added to card component

--- client/src/components/Screens/Cards/Cards.js
+++ client/src/components/Screens/Cards/Cards.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { Card, Input, Button } from 'antd';
 import { UserOutlined, PhoneOutlined, LockOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 import './Cards.css';
 
 
@@ -14,7 +15,10 @@ const Cards = props => {
             {props.isSignUp && <Input size='large' placeholder='Enter Phone Number' className='input' prefix={<PhoneOutlined />} />}
             <Input.Password size='large' placeholder='Enter Password' className='input' prefix={<LockOutlined />} />
             {props.isSignUp && <Input.Password size='large' placeholder='Confirm Password' className='input' prefix={<LockOutlined />} />}
-            <Button type='primary' size='large' className='actionButton' loading={isLoading}>{props.isSignUp ? 'Sign Up' : 'Login'}</Button>
+            <div className='buttonDiv'>
+                <Button type='primary' size='large' className='actionButton' loading={isLoading}>{props.isSignUp ? 'Sign Up' : 'Login'}</Button>
+                {props.isSignUp ? <Link to='/login'>Already have an account? Login!</Link> : <Link to='/signup'>Don't have an account? Sign Up!</Link>}
+            </div>
         </Card>
     )
 }
