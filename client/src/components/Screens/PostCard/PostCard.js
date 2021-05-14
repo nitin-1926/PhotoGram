6378f12bb1ce592dd2a118064ce36b4a1bb3b9b7
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2021-05-14 13:55:50 +0530

    Post card UI

--- client/src/components/Screens/PostCard/PostCard.js
+++ client/src/components/Screens/PostCard/PostCard.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Avatar } from 'antd';
 import './PostCard.css';
 
 const { Meta } = Card;
 
 const PostCard = props => {
+
+    // <img alt='profile' src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png' className='titleImage'></img>
+
+    // src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+
     return (
         <Card
             hoverable
-            style={{ width: 240 }}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            className='mainCard'
+            bodyStyle={{padding: '24px 0px'}}
         >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
+            <Meta className='cardHeader' avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} title='Europe Street beat' />
+            <div className='postImageDiv'>
+                <img alt='post' className='postImage' src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png' />
+            </div>
+            <div className='cardActionsDiv'>
+                Comments section
+            </div>
         </Card>
     );
 };
