2b64684e31e5dca5b6a0c59c0c2771cc079e98f0
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2021-05-15 19:50:10 +0530

    Getting all user details for signup

--- client/src/components/Screens/Cards/Cards.js
+++ client/src/components/Screens/Cards/Cards.js
@@ -6,6 +6,11 @@ import './Cards.css';
 
 
 const Cards = props => {
+    const [name, setName] = useState(null);
+    const [emailId, setEmailId] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [confirmPassword, setConfirmPassword] = useState(null);
+    const [phoneNumber, setPhoneNumber] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
     return (
@@ -15,14 +20,59 @@ const Cards = props => {
             className='detailsCard'
             headStyle={{ fontSize: '35px', fontFamily: 'Grand Hotel, cursive' }}
         >
-            <Input size='large' placeholder='Enter Email' prefix={<UserOutlined />} />
-            {props.isSignUp && <Input size='large' placeholder='Enter Name' className='input' prefix={<UserOutlined />} />}
-            {props.isSignUp && <Input size='large' placeholder='Enter Phone Number' className='input' prefix={<PhoneOutlined />} />}
-            <Input.Password size='large' placeholder='Enter Password' className='input' prefix={<LockOutlined />} />
-            {props.isSignUp && <Input.Password size='large' placeholder='Confirm Password' className='input' prefix={<LockOutlined />} />}
+            <Input
+                placeholder='Enter Email'
+                value={emailId}
+                size='large'
+                prefix={<UserOutlined />}
+                onChange={e => setEmailId(e.target.value)}
+            />
+            {props.isSignUp && <Input
+                placeholder='Enter Name'
+                value={name}
+                onChange={e => setName(e.target.value)}
+                size='large'
+                className='input'
+                prefix={<UserOutlined />}
+            />}
+            {props.isSignUp && <Input
+                placeholder='Enter Phone Number'
+                value={phoneNumber}
+                onChange={e => setPhoneNumber(e.target.value)}
+                size='large'
+                className='input'
+                prefix={<PhoneOutlined />}
+            />}
+            <Input.Password
+                placeholder='Enter Password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                size='large'
+                className='input'
+                prefix={<LockOutlined />}
+            />
+            {props.isSignUp && <Input.Password
+                placeholder='Confirm Password'
+                value={confirmPassword}
+                onChange={e => setConfirmPassword(e.target.value)}
+                size='large'
+                className='input'
+                prefix={<LockOutlined />}
+            />}
             <div className='buttonDiv'>
-                <Button type='primary' size='large' className='actionButton' loading={isLoading}>{props.isSignUp ? 'Sign Up' : 'Login'}</Button>
-                {props.isSignUp ? <Link to='/login'>Already have an account? Login!</Link> : <Link to='/signup'>Don't have an account? Sign Up!</Link>}
+                <Button
+                    type='primary'
+                    size='large'
+                    className='actionButton'
+                    onClick={() => props.handleSubmitButtonClick(emailId, password, name, phoneNumber)}
+                >
+                    {props.isSignUp ? 'Sign Up' : 'Login'}
+                </Button>
+                {props.isSignUp ? <Link to='/login'>
+                        Already have an account? Login!
+                    </Link> : <Link to='/signup'>
+                        Don't have an account? Sign Up!
+                </Link>}
             </div>
         </Card>
     )
--- client/src/components/Screens/SignUp/SignUp.js
+++ client/src/components/Screens/SignUp/SignUp.js
@@ -3,9 +3,14 @@ import Cards from '../Cards/Cards';
 import './SignUp.css';
 
 const SignUp = () => {
+
+    const signUpUser = (emailId, password, name, phoneNumber) => {
+        console.log(emailId, password, name, phoneNumber);
+    }
+
     return (
         <div className='cardContainer'>
-            <Cards isSignUp={true} />
+            <Cards isSignUp={true} handleSubmitButtonClick={signUpUser}/>
         </div>
     )
 }
