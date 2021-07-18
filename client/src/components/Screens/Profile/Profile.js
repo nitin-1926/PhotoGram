16277ba90bf06e3ce07829bb2a58e61891030457
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2021-07-18 13:14:44 +0530

    Getting the user profile posts

--- client/src/components/Screens/Profile/Profile.js
+++ client/src/components/Screens/Profile/Profile.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PostCard from '../PostCard/PostCard';
 import './Profile.css';
 
 const Profile = () => {
+
+    const [userPosts, setUserPosts] = useState([]);
+
+    useEffect(() => {
+        fetch('myPosts', {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+            }
+        }).then(res => res.json())
+            .then(data => {
+                console.log('Data: ', data);
+                setUserPosts(data.posts);
+            });
+    }, []);
+
+    // <img alt='Profile Pics' className='galleryPhotos'
+    //     src='https://images.unsplash.com/photo-1459356979461-dae1b8dcb702?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ym95fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    // />
+
     return (
         <div style={{maxWidth: '70%', margin: '0px auto'}}>
             <div style={{display: 'flex', justifyContent: 'space-around', margin: '18px 0px', borderBottom: '1px solid grey'}}>
@@ -21,24 +41,15 @@ const Profile = () => {
             </div>
 
             <div className='gallery'>
-                <img alt='Profile Pics' className='galleryPhotos'
-                    src='https://images.unsplash.com/photo-1459356979461-dae1b8dcb702?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ym95fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-                />
-                <img alt='Profile Pics' className='galleryPhotos'
-                    src='https://images.unsplash.com/photo-1459356979461-dae1b8dcb702?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ym95fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-                />
-                <img alt='Profile Pics' className='galleryPhotos'
-                    src='https://images.unsplash.com/photo-1459356979461-dae1b8dcb702?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ym95fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-                />
-                <img alt='Profile Pics' className='galleryPhotos'
-                    src='https://images.unsplash.com/photo-1459356979461-dae1b8dcb702?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ym95fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-                />
-                <img alt='Profile Pics' className='galleryPhotos'
-                    src='https://images.unsplash.com/photo-1459356979461-dae1b8dcb702?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ym95fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-                />
-                <img alt='Profile Pics' className='galleryPhotos'
-                    src='https://images.unsplash.com/photo-1459356979461-dae1b8dcb702?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ym95fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-                />
+                {userPosts && userPosts.length > 0 && userPosts.map(postData => {
+                    return <PostCard
+                        bodyStyle={{padding: '12px 0px 0px'}}
+                        title={postData.postedBy.name}
+                        photoUrl={postData.photoUrl}
+                        userId={postData.postedBy.emailId}
+                        caption={postData.caption}
+                    />
+                })}
             </div>
         </div>
     )
